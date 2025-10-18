1a9ee70cb342e50aa4e2f315dbf6a522df57e1f0
fullstackApp <princeabrahams777@gmail.com>
2025-10-18 12:50:43 +0200

    The Event app

---
+++ backend/src/main/java/com/myeventsapp/MyEventsApplication.java
@@ -0,0 +1,11 @@
+package com.myeventsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyEventsApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(MyEventsApplication.class, args);
+  }
+}
